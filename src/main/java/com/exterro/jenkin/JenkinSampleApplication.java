2fac843dec5f7ea981cdae8b11dab7d54b4f5251
MirdulaRangasamy <mirdula88@gmail.com>
2025-08-14 16:03:55 +0530

    war configuration

--- src/main/java/com/exterro/jenkin/JenkinSampleApplication.java
+++ src/main/java/com/exterro/jenkin/JenkinSampleApplication.java
@@ -4,11 +4,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
-public class JenkinSampleApplication {
+public class JenkinSampleApplication extends SpringBootServletInitializer{
 	public static Logger logger = LoggerFactory.getLogger(JenkinSampleApplication.class);
 	
 	public static void main(String[] args) {
@@ -17,6 +19,10 @@ public class JenkinSampleApplication {
 		SpringApplication.run(JenkinSampleApplication.class, args);
 	}
 	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(JenkinSampleApplication.class);
+    }
 	@PostConstruct
 	public void init() {
 		logger.info("In Sample Jenkins application started - Second Commit");
